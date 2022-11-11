52576c6416934743830ff4906187a5a1560eb9c9
Rishabh Gupta <nameisrishabh@gmail.com>
2022-11-11 11:03:55 +0530

    fixes

---
+++ build/precache-manifest.7c48764d9bb6eaa3091380105bf16bdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e345570739debbac6483",
+    "url": "./static/css/main.63de3074.chunk.css"
+  },
+  {
+    "revision": "e345570739debbac6483",
+    "url": "./static/js/main.c74ce163.chunk.js"
+  },
+  {
+    "revision": "b3c4f2f8c8e061940f2f",
+    "url": "./static/js/runtime~main.df8e1e2a.js"
+  },
+  {
+    "revision": "ae3ae4aec325bdcdbfb5",
+    "url": "./static/css/2.febf6cf2.chunk.css"
+  },
+  {
+    "revision": "ae3ae4aec325bdcdbfb5",
+    "url": "./static/js/2.b5df671f.chunk.js"
+  },
+  {
+    "revision": "d03d05430b9a42cc73e3",
+    "url": "./static/js/3.06a4cbf0.chunk.js"
+  },
+  {
+    "revision": "52e4679df90a4966e992cc429c937dee",
+    "url": "./index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -58,49 +58,49 @@ const App = () => {
 		<ScrollToTop>
 			<Routes>
 				{/* <Route path={process.env.PUBLIC_URL + "/"} element={<Splash />}/> */}
-				<Route path={process.env.PUBLIC_URL + "/"} element={<DigitalAgency />}/>
+				<Route path={"/"} element={<DigitalAgency />}/>
 
-				<Route path={process.env.PUBLIC_URL + "/digital-agency"} element={<DigitalAgency />}/>
-				<Route path={process.env.PUBLIC_URL + "/creative-agency"} element={<CreativeAgency />}/>
-				<Route path={process.env.PUBLIC_URL + "/personal-portfolio"} element={<PersonalPortfolio />}/>
-				<Route path={process.env.PUBLIC_URL + "/home-startup"} element={<HomeStartup />}/>
-				<Route path={process.env.PUBLIC_URL + "/corporate-agency"} element={<CorporateAgency />}/>
+				<Route path={"/digital-agency"} element={<DigitalAgency />}/>
+				<Route path={"/creative-agency"} element={<CreativeAgency />}/>
+				<Route path={"/personal-portfolio"} element={<PersonalPortfolio />}/>
+				<Route path={"/home-startup"} element={<HomeStartup />}/>
+				<Route path={"/corporate-agency"} element={<CorporateAgency />}/>
 
 				{/* Blogs */}
-				<Route path={process.env.PUBLIC_URL + "/blog-grid/"} element={<BlogGridView />}/>
-				<Route path={process.env.PUBLIC_URL + "/category/:slug"} element={<BlogCategory />}/>
-				<Route path={process.env.PUBLIC_URL + "/archive/:slug"} element={<BlogArchive />}/>
-				<Route path={process.env.PUBLIC_URL + "/blog-details/:id"} element={<BlogDetails />}/>
+				<Route path={"/blog-grid/"} element={<BlogGridView />}/>
+				<Route path={"/category/:slug"} element={<BlogCategory />}/>
+				<Route path={"/archive/:slug"} element={<BlogArchive />}/>
+				<Route path={"/blog-details/:id"} element={<BlogDetails />}/>
 
 				{/* Service */}
-				<Route path={process.env.PUBLIC_URL + "/service-one/"} element={<ServiceOne />}/>
-				<Route path={process.env.PUBLIC_URL + "/service-two/"} element={<ServiceTwo />}/>
-				<Route path={process.env.PUBLIC_URL + "/service-details/:slug"} element={<ServiceDetails />}/>
+				<Route path={"/service-one/"} element={<ServiceOne />}/>
+				<Route path={"/service-two/"} element={<ServiceTwo />}/>
+				<Route path={"/service-details/:slug"} element={<ServiceDetails />}/>
 
 				{/* Project  */}
-				<Route path={process.env.PUBLIC_URL + "/project-grid-one"} element={<ProjectGridOne />}/>
-				<Route path={process.env.PUBLIC_URL + "/project-grid-two"} element={<ProjectGridTwo />}/>
-				<Route path={process.env.PUBLIC_URL + "/project-grid-three"} element={<ProjectGridThree />}/>
-				<Route path={process.env.PUBLIC_URL + "/project-width-one"} element={<ProjectGridFour />}/>
-				<Route path={process.env.PUBLIC_URL + "/project-width-two"} element={<ProjectGridFive />}/>
-				<Route path={process.env.PUBLIC_URL + "/project-details/:slug"} element={<ProjectDetails />}/>
+				<Route path={"/project-grid-one"} element={<ProjectGridOne />}/>
+				<Route path={"/project-grid-two"} element={<ProjectGridTwo />}/>
+				<Route path={"/project-grid-three"} element={<ProjectGridThree />}/>
+				<Route path={"/project-width-one"} element={<ProjectGridFour />}/>
+				<Route path={"/project-width-two"} element={<ProjectGridFive />}/>
+				<Route path={"/project-details/:slug"} element={<ProjectDetails />}/>
 
 				{/* Pages  */}
-				<Route path={process.env.PUBLIC_URL + "/about-us"} element={<AboutUs />}/>
-				<Route path={process.env.PUBLIC_URL + "/our-office"} element={<OurOffice />}/>
-				<Route path={process.env.PUBLIC_URL + "/our-clients"} element={<OurClients />}/>
-				<Route path={process.env.PUBLIC_URL + "/team"} element={<Team />}/>
-				<Route path={process.env.PUBLIC_URL + "/team-details/:slug"} element={<TeamDetails />}/>
-				<Route path={process.env.PUBLIC_URL + "/case-study"} element={<CaseStudy />}/>
-				<Route path={process.env.PUBLIC_URL + "/case-details/:slug"} element={<CaseDetails />}/>
-				<Route path={process.env.PUBLIC_URL + "/testimonials"} element={<Testimonials />}/>
-				<Route path={process.env.PUBLIC_URL + "/pricing-table"} element={<PricingTable />}/>
-				<Route path={process.env.PUBLIC_URL + "/typography"} element={<Typography />}/>
-				<Route path={process.env.PUBLIC_URL + "/contact"} element={<Contact />}/>
-				<Route path={process.env.PUBLIC_URL + "/404"} element={<ErrorPage />}/>
-				<Route path={process.env.PUBLIC_URL + "/coming-soon"} element={<ComingSoon />}/>
-				<Route path={process.env.PUBLIC_URL + "/privacy-policy"} element={<PrivacyPolicy />}/>
-				<Route path={process.env.PUBLIC_URL + "/terms-use"} element={<TermsOfUse />}/>
+				<Route path={"/about-us"} element={<AboutUs />}/>
+				<Route path={"/our-office"} element={<OurOffice />}/>
+				<Route path={"/our-clients"} element={<OurClients />}/>
+				<Route path={"/team"} element={<Team />}/>
+				<Route path={"/team-details/:slug"} element={<TeamDetails />}/>
+				<Route path={"/case-study"} element={<CaseStudy />}/>
+				<Route path={"/case-details/:slug"} element={<CaseDetails />}/>
+				<Route path={"/testimonials"} element={<Testimonials />}/>
+				<Route path={"/pricing-table"} element={<PricingTable />}/>
+				<Route path={"/typography"} element={<Typography />}/>
+				<Route path={"/contact"} element={<Contact />}/>
+				<Route path={"/404"} element={<ErrorPage />}/>
+				<Route path={"/coming-soon"} element={<ComingSoon />}/>
+				<Route path={"/privacy-policy"} element={<PrivacyPolicy />}/>
+				<Route path={"/terms-use"} element={<TermsOfUse />}/>
 
 			</Routes>
 		</ScrollToTop>
